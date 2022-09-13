4c603f93d577e7d7b6c79ad19d57814280916d3a
taylora-c <91395907+taylora-c@users.noreply.github.com>
2022-09-13 18:31:47 -0400

    Add files via upload
    Fix bugs via testing on swaggerhub

--- HootLogin.php
+++ HootLogin.php
@@ -3,24 +3,24 @@
 	$inData = getRequestInfo();
 	
 	$id = 0;
-	$firstName = "";
-	$lastName = "";
+	$username = "";
+	$password = "";
 
-	$conn = new mysqli("localhost", "digital", "hootDB", "cop4331-g3"); 	
+    $conn = new mysqli("localhost", "admin", "my_password", "testdb"); 
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt = $conn->prepare("SELECT ID,username,password FROM Users WHERE username=? AND password =?");
+		$stmt->bind_param("ss", $inData["username"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+			returnWithInfo( $row['username'], $row['password'], $row['ID'] );
 		}
 		else
 		{

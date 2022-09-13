1590958e9075605fcaee8803b40ca1695cb199ae
Marc-Anthony Cross <84754983+carcmross@users.noreply.github.com>
2022-09-12 23:32:40 -0400

    Updated to make sure error message works.

--- Delete.php
+++ Delete.php
@@ -3,20 +3,25 @@
 	$inData = getRequestInfo();
 
 	$username = $inData['username'];
+	$firstname = $inData['firstname'];
 	$phonenumber = $inData['phonenumber'];
 
-	$conn = new mysqli("localhost", "digital", "hootDB", "COP4331"); 	
+	$conn = new mysqli("localhost", "admin", "testing123", "COP4331"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		if ( $conn->query("DELETE FROM Contacts WHERE username = " . $username . " and phonenumber = " . $phonenumber) )
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE username = ? AND firstname = ? AND phonenumber = ?");
+		$stmt->bind_param("sss", $username, $firstname, $phonenumber);
+		$stmt->execute();
+		$stmt->store_result();
+		if ($stmt->affected_rows > 0)
 			echo "Contact deleted successfully.";
 		else
 			returnWithError("Contact cannot be deleted because it does not exist.");
-
+		$stmt->close();
 		$conn->close();
 	}
 	
